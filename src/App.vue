aaa85aa3621b087acce9a2f8b7e374bbc3ea4979
Mero <gjn_2012@foxmail.com>
2021-07-10 23:09:20 +0800

    testing on-change then validate

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 <Form :model="d" :rules="rulesObj">
   <FormItem label="start" prop="d0">
-    <Date-picker v-model="d.d0" clearable
-                 @on-change="dtrChange" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
+    <Date-picker v-model="d.d0" clearable :editable="false"
+                 @on-change="dtrChange(true)" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
   </FormItem>
-  <Form-item label="end" prop="d1">
-    <Date-picker v-model="d.d1" clearable
-                 @on-change="dtrChange1" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
-  </Form-item>
+  <FormItem label="end" prop="d1">
+    <Date-picker v-model="d.d1" clearable :editable="false"
+                 @on-change="dtrChange(false)" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
+  </FormItem>
 </Form>
   </div>
 </template>
@@ -32,24 +32,47 @@ export default {
         d1: [
           {
             validator: (r, v, cb) => {
-              alert('d1 validating')
-              if (v && v[0] === v[1]) {
-                return cb()
-              } else {
-                return cb(new Error('YOU SHALL NOT PASS'))
+              console.log(v)
+              if (!v || !v[0] || !v[1]) {
+                alert('d1 validating 不能为空')
+                cb(new Error('不能为空'))
+                // return
               }
+              if (v[0].getFullYear() !== v[1].getFullYear() ||
+                      v[0].getMonth() !== v[1].getMonth() ||
+                      v[0].getDate() !== v[1].getDate()
+              ) {
+                alert('d1 validating Y S N P')
+                cb(new Error('YOU SHALL NOT PASS'))
+                // return
+              }
+              alert('VALID')
+              cb()
             },
             trigger: 'change'
           }
         ]
       },
-      d: {d0: [], d1:[]}
+      d: {
+        d0: [],
+        d1: []
+      }
     }
   },
   methods: {
-    dtrChange(a){
-      alert(JSON.stringify(a))
-      this.d.d1=[new Date(new Date(a[0]).getTime() + 1000*60*60), new Date(new Date(a[0]).getTime() + 3000*60*60)]
+    dtrChange(isStart){
+      const a = isStart ? this.d.d0 : this.d.d1
+      if (isStart) {
+        this.d.d1=[
+          new Date(new Date(a[0]).getTime() + 8000*60*60),
+          new Date(new Date(a[1]).getTime() + 8000*60*60)
+        ]
+      } else {
+        this.d.d0=[
+          new Date(new Date(a[0]).getTime() - 8000*60*60),
+          new Date(new Date(a[1]).getTime() - 8000*60*60)
+        ]
+      }
     },
     dtrChange1(a) {
       alert('second' + JSON.stringify(a))
