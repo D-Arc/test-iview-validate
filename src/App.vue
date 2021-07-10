68058280314f13e532e507b814e7a9581eab1fcf
Mero <gjn_2012@foxmail.com>
2021-07-10 22:08:45 +0800

    test code

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Form :model="d" :rules="rulesObj">
+  <FormItem label="start" prop="d0">
+    <Date-picker v-model="d.d0" clearable
+                 @on-change="dtrChange" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
+  </FormItem>
+  <Form-item label="end" prop="d1">
+    <Date-picker v-model="d.d1" clearable
+                 @on-change="dtrChange1" type="datetimerange" confirm placeholder="选择日期" style="width: 400px"></Date-picker>
+  </Form-item>
+</Form>
   </div>
 </template>
 
@@ -12,6 +22,38 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      rulesObj: {
+        d0: [
+          { required: true, message: '不能为空', trigger: 'change' }
+        ],
+        d1: [
+          {
+            validator: (r, v, cb) => {
+              alert('d1 validating')
+              if (v && v[0] === v[1]) {
+                return cb()
+              } else {
+                return cb(new Error('YOU SHALL NOT PASS'))
+              }
+            },
+            trigger: 'change'
+          }
+        ]
+      },
+      d: {d0: [], d1:[]}
+    }
+  },
+  methods: {
+    dtrChange(a){
+      alert(JSON.stringify(a))
+      this.d.d1=[new Date(new Date(a[0]).getTime() + 1000*60*60), new Date(new Date(a[0]).getTime() + 3000*60*60)]
+    },
+    dtrChange1(a) {
+      alert('second' + JSON.stringify(a))
+    }
   }
 }
 </script>
